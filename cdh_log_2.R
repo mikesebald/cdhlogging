0e7ab520f4c2485c467ea6cf4520403f35febb3b
mikesebald <michael.sebald@gmail.com>
2016-03-31 09:33:39 +0200

    log file loading optimized for speed

--- cdh_log_2.R
+++ cdh_log_2.R
@@ -1,20 +1,29 @@
 library(plotly)
 library(xlsx)
-#library(readr)
+library(scales)
+library(readr)
 
-# fr.weblog <- read_fwf(
-#   file = "../cdhlogging_data/server-2016-03-21.log",
-#   fwf_widths(c(5, 24, 7, 9, 0))
-# )
+setwd("e:/R/cdh_log")
 
-system.time(f.weblog <- read.fwf(
-  "data/server-2016-03-21.log",
-  widths = c(5, 24, 7, 9, 100),
-  col.names = c("One", "Timestamp", "Thread",
-                "Severity", "Message"),
-  comment.char = "",
-  stringsAsFactors = FALSE
-))
+# about 5 times slower:
+# system.time(f.weblog1 <- read.fwf(
+#   "../cdh_log_data/web.log",
+#   widths = c(5, 24, 7, 9, 100),
+#   col.names = c("One", "Timestamp", "Thread",
+#                 "Severity", "Message"),
+#   comment.char = "",
+#   stringsAsFactors = FALSE
+# ))
+
+system.time({
+  f.tmp <- read_lines("../cdh_log_data/web.log")
+  l.tmp <- lapply(f.tmp, function(x) substring(x, 
+                                               c(1, 6, 30, 37, 46), 
+                                               c(4, 28, 35, 45, 150)))
+  f.weblog <- as.data.frame(matrix(unlist(l.tmp), ncol = 5, byrow = TRUE),
+                            stringsAsFactors = FALSE)
+  colnames(f.weblog) <- c("One", "Timestamp", "Thread", "Severity", "Message")
+})
 
 
 cleans <- grepl("No changes detected:|Record saved:", f.weblog$Message)
@@ -24,19 +33,22 @@ f.weblog$Timestamp <- gsub(",", ".", f.weblog$Timestamp)
 f.weblog$Timestamp <- strptime(f.weblog$Timestamp,
                                format = "%Y-%m-%d %H:%M:%OS")
 
+packagesize <- 1000
+
+
 
 # Restart from here
 
 weblog <- f.weblog
 
-groups <- rep(1:5000, each = 1000)
+groups <- rep(1:5000, each = packagesize)
 
 weblog <- cbind(weblog, groups[1:nrow(weblog)])
 
 splitted.weblog <- split(weblog, weblog[, 6])
 
-min.ts <- do.call(c, lapply(splitted.weblog, function(x) min(x$Timestamp)))
-max.ts <- do.call(c, lapply(splitted.weblog, function(x) max(x$Timestamp)))
+min.ts <- do.call(c, lapply(splitted.weblog, function(x) x[1, "Timestamp"]))
+max.ts <- do.call(c, lapply(splitted.weblog, function(x) x[nrow(x), "Timestamp"]))
 min.ts.c <- as.character(min.ts, format = "%H:%M:%OS3")
 max.ts.c <- as.character(max.ts, format = "%H:%M:%OS3")
 diff.ts <- as.numeric(difftime(max.ts, min.ts, units = "secs"))
@@ -47,23 +59,28 @@ sav.log <- do.call(c, lapply(splitted.weblog,
 noc.log <- do.call(c, lapply(splitted.weblog, 
                   function(x) sum(grepl("No changes detected:", x$Message))))
 
-df <- as.data.frame(cbind(min.ts.c, max.ts.c, diff.ts, rows.log, rps.log, 
-                          sav.log, noc.log))
-colnames(df) <- c("Start", "End", "Time", "Rows", "RowsPerSec", 
+df <- as.data.frame(cbind.data.frame(min.ts, min.ts.c, max.ts.c, diff.ts, rows.log, 
+                                     rps.log, sav.log, noc.log))
+colnames(df) <- c("Timestamp", "Start", "End", "Time", "Rows", "RowsPerSec", 
                   "Updates", "NoChange")
 
-# x <- list(title = "Paket")
-# y <- list(title = "Records pro Sekunde")
-# p <- plot_ly(data = df, y = RowsPerSec, mode = "markers") %>%
-#   layout(xaxis = x, yaxis = y)
-# p
+p3 <- ggplot(data = df, aes(x = Timestamp, y = RowsPerSec)) +
+  geom_point(aes(text = paste("Start time:", Start)), 
+             size = 2, 
+             colour = "darkgreen") +
+  scale_x_datetime(labels = date_format("%H:%M:%S")) +
+  xlab("Zeit") + ylab("Records pro Sekunde") +
+  geom_smooth(aes(colour = RowsPerSec, fill = RowsPerSec)) +
+  theme(plot.margin = unit(c(1,1,2,2), units = "lines"))
+p3
 
-df$RowsPerSec <- as.numeric(df$RowsPerSec)
-p2 <- ggplot(data = df, aes(x = as.numeric(rownames(df)), 
-                            y = RowsPerSec)) +
-  xlab("Paket") + ylab("Records pro Sekunde") +
-  geom_point(aes(text = paste("Start time:", Start)), size = 2) +
-  geom_smooth(aes(colour = RowsPerSec, fill = RowsPerSec))
-ggplotly(p2)
+layout(ggplotly(p3),
+       margin = list(t = 20,
+                     r = 20,
+                     b = 80,
+                     l = 80),
+       xaxis = list(title = "Zeit"),
+       yaxis = list(title = "Records pro Sekunde"))
 
-write.xlsx(df, file = "data/runtimes.xlsx")
+df.xls <- df[,-1]
+write.xlsx(df.xls, file = "../cdh_log_data/runtimes.xlsx")
